dd11189777ea80b38a5cef2c12616c5c5b7f2b84
NHEBotx <cocnammax@gmail.com>
2025-01-05 05:35:37 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const chalk = require('chalk');
+/*
+ * Create By Sych
+ * Follow https://github.com/NHEBotx
+ * Whatsapp : https://whatsapp.com/channel/0029Vb0v3F71yT264EejzJ3e
+ */
+//~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
+//settings owner di ./setown.js
+global.packname = 'MORELY-MD BOTz'
+global.owner = ["6288989971490"]
+global.botnum = ["62881027174423"]
+global.author = 'TNGX'
+global.owname = 'TngxAja'
+global.botname = 'MORELY-MD BOTz'
+global.themeemoji = '🪀'
+global.f = '> '
+global.n = '`'
+global.videoMenu = fs.readFileSync('./MorelyMedia/menu/sych.mp4');
+global.listv = ['⛏', '●', '■', '✿', '▲', 'ଳ', 'ϟ', '✶', '➤', '✦', '✧', '△', '❀', '⋆𖦹', '□', '𖤓', 'ᨒ', '◇', '𖣂', '々', '〆', 'ᯓ★', '꩜', '✮']
+global.emot = ['🌱', '🌻', '🌞', '❄️', '🌿', '💫', '⭐', '🍃', '🔥', '⚡', '🫧', '🌵', '🪺', '🪨', '🪵', '🌪️', '🍄']
+global.tempatDB = 'database.json'
+global.pairing_code = true
+global.fake = {
+    tmenu: 'https://i.ibb.co.com/7bVhfhb/3ed349054e5c77cfbebf58293d1e0df0.jpg',
+	texz: `${botname}`,
+	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
+	thumbnailUrl: 'https://i.ibb.co.com/3rqCPX6/fk.jpg',
+	thumbnail: fs.readFileSync('./src/media/sych.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/pdf'],
+}
+global.my = {
+	yt: 'Gaada yt, Ga doyan upload di YOUTUBE',
+	gh: 'https://github.com/NHEBotx',
+	gc: 'https://chat.whatsapp.com/Bc0N1yYfNwI25RMDOrrwjg',
+	ch: 'https://whatsapp.com/channel/0029Vb2kT5pJ3jv5yIVMsx2q',
+}
+global.limit = {
+	free: 999,
+	premium: 999,
+	vip: 'VIP'
+}
+global.uang = {
+	free: 100000,
+	premium: 1000000,
+	vip: 10000000
+}
+global.mess = {
+	key0: 'Apikey mu telah habis silahkan kunjungi\nhttps://my.hitori.pw',
+	owner: 'lu bkn owner',
+	admin: 'lu bkn atmin',
+	botAdmin: 'gw bkn atmin bro',
+	group: 'cma buat grup aj',
+	private: 'cm di prvt cht',
+	prem: 'lu bkn user premium',
+	wait: 'bentar duluu',
+	error: 'eror bro!',
+	done: 'udh lunas yh'
+}
+global.APIs = {
+	hitori: 'https://my.hitori.pw/api',
+}
+global.APIKeys = {
+	'https://my.hitori.pw/api': 'htrkey-awokawok',
+}
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
